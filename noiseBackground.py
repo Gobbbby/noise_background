9003754b2c94a6494e6b1bfc04d530fced35279d
Gobbbby <58797026+Gobbbby@users.noreply.github.com>
2020-09-30 16:47:40 -0400

    added the actual python file

---
+++ noiseBackground.py
@@ -0,0 +1,75 @@
+
+import numpy as np
+import noise
+from sys import stdout
+
+
+
+class noiseBackground:
+    def __init__(self, size=48, size2=None):
+        self.size = (size, size) if not size2 else (size2, size)
+        self.scale = 0.5
+        self.coordGrids = np.meshgrid(np.linspace(0, 1, self.size[1]), np.linspace(0, 1, self.size[0]))
+        self.transitionLoops = 40
+
+        self.noise = np.zeros(self.size)
+        self.prevNoise = np.zeros(self.size)
+        self.difference = np.zeros(self.size)
+
+        for _ in range(2): self.setNoise()
+
+        self.increments = [
+            '  ',
+            '\u001b[33m' '..' '\033[0;0m',
+            '\u001b[32m' '::' '\033[0;0m',
+            '\u001b[34m' 'xx' '\033[0;0m',
+            '\u001b[35m' 'XX' '\033[0;0m',
+            '\033[1;31m' '@@' '\033[0;0m',
+        ]
+        self.inRange = lambda n, l, h: l if n < l else n if n < h else h
+        self.incrNum = lambda x: self.inRange(int((x*10+6)/2), 0, 5)
+
+
+    def setNoise(self):
+        self.prevNoise = self.noise
+
+        self.noise = np.vectorize(noise.pnoise2)(                               # applying perlin noise
+                                                 self.coordGrids[1]/self.scale,
+                                                 self.coordGrids[0]/self.scale,
+                                                 octaves=1,
+                                                 persistence=0.5,
+                                                 lacunarity=2.0,
+                                                 repeatx=self.size[0],
+                                                 repeaty=self.size[0],
+                                                 base=np.random.randint(0,100)
+                                                 )
+
+        self.difference = self.noise - self.prevNoise
+        self.difference /= self.transitionLoops
+
+
+    def __str__(self):
+        string = '\n'
+        for row in self.prevNoise:
+            for cell in row:
+                try:
+                    string += self.increments[self.incrNum(cell)]
+                except:
+                    print(self.incrNum(cell))
+            string += '\n'
+        return string
+
+
+
+noiseB = noiseBackground(71, 43)
+
+stdout.write(f'\u001b[1J')                                  # clearing the screen
+stdout.write(f'\u001b[999A')                                # going to the top of the screen
+stdout.write('\u001b[s')                                    # saving the position
+
+while True:
+    for _ in range(noiseB.transitionLoops):
+        stdout.write(str(noiseB))
+        stdout.write('\u001b[u')                            # going back to the saved position
+        noiseB.prevNoise += noiseB.difference
+    noiseB.setNoise()
