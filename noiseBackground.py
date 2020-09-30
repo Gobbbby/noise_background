24df23ca97d39da8ac0900815ba0823572f3839a
Alex Macri <alexmacri04@gmail.com>
2020-09-30 18:48:31 -0400

    Added automatic sizing and misc improvements

--- noiseBackground.py
+++ noiseBackground.py
@@ -2,15 +2,16 @@
 import numpy as np
 import noise
 from sys import stdout
+from subprocess import check_output
 
 
 
 class noiseBackground:
-    def __init__(self, size=48, size2=None):
-        self.size = (size, size) if not size2 else (size2, size)
+    def __init__(self, rows, cols):
+        self.size = (rows, cols//2)             # dividing by 2 because each pixel takes up 2 characters
         self.scale = 0.5
         self.coordGrids = np.meshgrid(np.linspace(0, 1, self.size[1]), np.linspace(0, 1, self.size[0]))
-        self.transitionLoops = 40
+        self.transitionLoops = 100
 
         self.noise = np.zeros(self.size)
         self.prevNoise = np.zeros(self.size)
@@ -26,6 +27,7 @@ class noiseBackground:
             '\u001b[35m' 'XX' '\033[0;0m',
             '\033[1;31m' '@@' '\033[0;0m',
         ]
+
         self.inRange = lambda n, l, h: l if n < l else n if n < h else h
         self.incrNum = lambda x: self.inRange(int((x*10+6)/2), 0, 5)
 
@@ -39,7 +41,7 @@ class noiseBackground:
                                                  octaves=1,
                                                  persistence=0.5,
                                                  lacunarity=2.0,
-                                                 repeatx=self.size[0],
+                                                 repeatx=self.size[1],
                                                  repeaty=self.size[0],
                                                  base=np.random.randint(0,100)
                                                  )
@@ -61,11 +63,15 @@ class noiseBackground:
 
 
 
-noiseB = noiseBackground(71, 43)
+rows, cols = map(int, check_output(['stty', 'size']).split())       # getting the rows and columns of the terminal window, and converting them to integer
+
+noiseB = noiseBackground(rows-2, cols)                      # subtracting 2 because it "overshoots" by 2, in the case of this program (it starts scrolling)
 
 stdout.write(f'\u001b[1J')                                  # clearing the screen
 stdout.write(f'\u001b[999A')                                # going to the top of the screen
 stdout.write('\u001b[s')                                    # saving the position
+
+
 
 while True:
     for _ in range(noiseB.transitionLoops):
